81d79bc02beb585dea3aca0a151ba18de46d7707
jay_sun <jay.sun@tatum.io>
2021-12-07 16:53:40 +0800

    fix

--- multiToken-connector/module/src/dto/PathChain.ts
+++ multiToken-connector/module/src/dto/PathChain.ts
@@ -4,6 +4,6 @@ import {IsIn, IsNotEmpty} from 'class-validator';
 export class PathChain {
 
     @IsNotEmpty()
-    @IsIn([Currency.ETH, Currency.CELO, Currency.BSC, Currency.ONE, Currency.MATIC])
+    @IsIn([Currency.ETH, Currency.CELO, Currency.BSC, Currency.ONE, Currency.MATIC, Currency.KCS])
     public chain: Currency;
 }
--- nft-connector/module/src/dto/PathChain.ts
+++ nft-connector/module/src/dto/PathChain.ts
@@ -5,7 +5,7 @@ import {IsIn, IsNotEmpty} from 'class-validator';
 export class PathChain {
 
     @IsNotEmpty()
-    @IsIn([Currency.ETH, Currency.CELO, Currency.BSC, Currency.FLOW, Currency.XDC, Currency.TRON, Currency.ONE,
+    @IsIn([Currency.ETH, Currency.CELO, Currency.BSC, Currency.FLOW, Currency.XDC, Currency.TRON, Currency.ONE, Currency.KCS,
         Currency.MATIC, Currency.EGLD, C.SOL, Currency.ALGO])
     public chain: Currency;
 }
